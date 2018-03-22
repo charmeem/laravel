d26fb240bc9bd4180ecb60e21303a881a23c05a4
teenmeem <Mubashir@example.com>
2018-03-22 18:13:43 +0500

    Basic routing practised in web.php

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('test', function() {
+    return " Hi brother";
+});
+
+Route::get('home', function() {
+    return view('home');
+});
+
+Route::get('/product/{category}/{item}', function ($category, $item) {
+    return "Hi, Do you want to buy a Product " . $item . " from Category " . $category;
+});
