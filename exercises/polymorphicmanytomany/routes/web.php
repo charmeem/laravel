45d00e8f6ae48f9469fe8e473752f6536342a4c5
teenmeem <Mubashir@example.com>
2018-04-19 15:41:10 +0500

    my first web app in lara_cms

--- exercises/polymorphicmanytomany/routes/web.php
+++ exercises/polymorphicmanytomany/routes/web.php
@@ -20,12 +20,32 @@ Route::get('/', function () {
 });
 
 Route::get('/create', function(){
-    $post = Post::create(['name'=>'Mubashir first post']);
+    $post = Post::create(['name'=>'Mubashir last post']);
     $tag1 = Tag::find(1);
     $post->tags()->save($tag1);
-    
-    $video = Video::create(['name'=>'My first video']);
+
+    $video = Video::create(['name'=>'My last video']);
     $tag2 = Tag::find(2);
     $video->tags()->save($tag2);
-    
+
+});
+
+Route::get('/read', function(){
+   $post = Post::findOrFail(7);
+   foreach($post->tags as $tag){
+       echo $tag;
+   }
+});
+
+Route::get('/update', function(){
+    $post = Post::find(7);
+    foreach($post->tags as $tag){
+        $tag->whereName('Laravel')->update(['name'=>'PHP']);
+    }
+});
+
+
+Route::get('/delete', function(){
+    $post=Post::findOrFail(1);
+    $post->delete();
 });
